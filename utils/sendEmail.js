2b480a6d8fbea6a887ff7b1719e748695cc6d8e8
phantomjac <drchesireyang@gmail.com>
2025-12-15 20:15:37 +0300

    Feat: Email Sending via SMTP Fix

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,20 +1,14 @@
-import nodemailer from "nodemailer";
+import sgMail from "@sendgrid/mail";
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export const sendEmail = async ({ to, subject, html }) => {
- const MAIL_FROM = process.env.MAIL_FROM || "vervehubwriteups@gmail.com";
-  const transporter = nodemailer.createTransport({
-    host: "smtp.sendgrid.net",
-    port: 587,          
-    secure: false,      
-    auth: {
-      user: "apikey",   
-      pass: process.env.SENDGRID_API_KEY, // your SendGrid API key
-    },
-  });
+  const MAIL_FROM =
+    process.env.MAIL_FROM || "no-reply@vervehub.com";
 
-  await transporter.sendMail({
-    from: MAIL_FROM, 
+  await sgMail.send({
     to,
+    from: MAIL_FROM,
     subject,
     html,
   });
